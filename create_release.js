e7f9f87e111b7c130a9667b45c02c495a9e00179
Timo Molls <timo.molls@enet.eu>
2024-02-26 16:33:17 +0100

    Added create_release.js

---
+++ create_release.js
@@ -0,0 +1,69 @@
+const {Octokit} = require("octokit");
+
+if (!process.argv[2] || !process.argv[3]) {
+    throw new Error("Argument required")
+}
+const octokit = new Octokit()
+const auth_octokit = new Octokit({
+    auth: process.argv[2]
+});
+
+/**
+ *
+ * @param response{OctokitResponse}
+ */
+function ensure_status_code(response) {
+    if (response.status !== 200) {
+        throw new Error("Request failed");
+    }
+}
+
+async function get_release_infos(version) {
+    const response =  await octokit.request('GET /repos/{owner}/{repo}/releases/tags/{tag}', {
+        owner: 'bo4e',
+        repo: 'BO4E-Schemas',
+        tag: version
+    });
+    ensure_status_code(response);
+    return response;
+}
+
+async function get_latest_release_tag() {
+    const response = await octokit.request('GET /repos/{owner}/{repo}/releases/latest', {
+        owner: 'bo4e',
+        repo: 'BO4E-Schemas'
+    });
+    ensure_status_code(response);
+    return response.data['tag_name'];
+}
+
+/**
+ *
+ * @param version {string}
+ * @param release_infos {string[]}
+ * @param is_latest {boolean}
+ * @returns {Promise<void>}
+ */
+async function create_release(version, release_infos, is_latest) {
+    const response = await auth_octokit.request('POST /repos/{owner}/{repo}/releases', {
+        owner: 'TimoMolls',
+        repo: 'BO4E-Java',
+        tag_name: version,
+        target_commitish: 'master',
+        name: release_infos['name'],
+        body: release_infos['body'],
+        draft: false,
+        prerelease: release_infos['prerelease'],
+        make_latest: is_latest
+    });
+    ensure_status_code(response);
+    return response.data;
+}
+
+function main(version) {
+    const release_infos = get_release_infos(version);
+    const is_latest = version === get_latest_release_tag();
+    create_release(version, release_infos, is_latest).then(console.log);
+}
+
+main(process.argv[3]);

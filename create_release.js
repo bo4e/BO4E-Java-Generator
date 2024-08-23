31c0d66bd85c2c03b127d886e5ecc866aa3e80d4
Timo Molls <timo.molls@enet.eu>
2024-08-23 13:07:13 +0200

    updated create_release.js

--- create_release.js
+++ create_release.js
@@ -45,14 +45,20 @@ async function get_latest_release_tag() {
  * @param release_infos {string[]}
  * @param is_prerelease {boolean}
  * @param is_latest {boolean}
+ * @param addon {string|null}
  * @returns {Promise<void>}
  */
-async function create_release(version, release_infos, is_prerelease, is_latest) {
+async function create_release(version, release_infos, is_prerelease, is_latest, addon) {
+    let target = 'master';
+    if (addon != null) {
+        version += "-wd"
+        target = 'withDependencies';
+    }
     const response = await auth_octokit.request('POST /repos/{owner}/{repo}/releases', {
         owner: 'TimoMolls',
         repo: 'BO4E-Java',
         tag_name: version,
-        target_commitish: 'master',
+        target_commitish: target,
         name: release_infos['name'],
         body: release_infos['body'],
         draft: false,
@@ -62,13 +68,17 @@ async function create_release(version, release_infos, is_prerelease, is_latest) 
     ensure_status_code(response, 201);
 }
 
-async function main(version) {
+async function main(version, addon = null) {
     const release_infos = await get_release_infos(version);
-    const is_prerelease = (release_infos['prerelease'] || version.includes('-rc'))
+    const is_prerelease = (release_infos['prerelease'] || version.includes('-rc') || addon != null)
     const latest_release_tag = await get_latest_release_tag();
     const latest_is_prerelease = latest_release_tag.includes('-rc')
     const is_latest = !is_prerelease && (latest_is_prerelease || version === latest_release_tag);
-    await create_release(version, release_infos, is_prerelease, is_latest);
+    await create_release(version, release_infos, is_prerelease, is_latest, addon);
 }
 
-main(process.argv[3]).then();
+if (process.argv[4]) {
+    main(process.argv[3], process.argv[4]).then();
+} else {
+    main(process.argv[3]).then();
+}
